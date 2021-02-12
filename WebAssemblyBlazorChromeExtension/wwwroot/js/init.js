ed3a3a0041919806194a340646ba40e3d1745729
Waleed Hafeez <waleed.hafeez@dev01.polyvista.net>
2021-02-12 11:37:45 +0500

    Add project files.

---
+++ WebAssemblyBlazorChromeExtension/wwwroot/js/init.js
@@ -0,0 +1,37 @@
+﻿(function () {
+    //alert("Hello from Init.js i.e; content_script");
+    $.get(chrome.runtime.getURL('index.html'), function (data) {
+        console.log(data);
+        //debugger
+        $(data).appendTo('#rhs');
+       // $(data).appendTo('._8esl');
+        // Or if you're using jQuery 1.8+:
+        // $($.parseHTML(data)).appendTo('body');
+    });
+})();
+
+//(function () {
+//    function fnAddButtons() {
+//        var btn = document.createElement("input");
+//        btn.value = "Search apple-watches";
+//        btn.id = "search-mm-btn";
+//        btn.type = "submit";
+//        document.querySelector(".FPdoLc.tfB0Bf center").appendChild(btn);
+//    }
+
+//    function fnDefineEvents() {
+//        document
+//            .getElementById("search-mm-btn")
+//            .addEventListener("click", function (event) {
+//                fnSearch(event.target.value.split(" ")[1]);
+//            });
+//    }
+//    function fnSearch(str) {
+//        document.querySelector(".gLFyf.gsfi").value = str;
+//    }
+
+   
+
+//    fnAddButtons();
+//    fnDefineEvents();
+//})();
---
+++ WebAssemblyBlazorChromeExtension/wwwroot/js/site.js
@@ -0,0 +1,7 @@
+﻿function showAlert(message) {
+    alert(message);
+}
+
+//$.getJSON("chrome-extension://abdoflfodcjhjhmnknkhpbkgfodoelab/framework/blazor.boot.json", function (json) {
+//    console.log(json); // this will show the info it in firebug console
+//});
